6e2d90936573898686c92ccf2e2137debb58256b
cc12210 <2832913976@qq.com>
2018-10-11 14:51:17 +0800

    轮播图功能完善

--- js/shouye.js
+++ js/shouye.js
@@ -2,10 +2,14 @@ var swiper = (function () {
     return {
         init: function () {
             this.bannerImg = document.querySelectorAll('.banner_img');
-            console.log(this.bannerImg);
             this.circle_li = document.querySelector('.circle_box');
+            this.banner = document.querySelector('#banner');
+            this.circle_liAll = this.circle_li.children;
             this.index = 0;
+            this.timer = null;
             this.event();
+            this.autoplay();
+            this.stopplay();
         },
         event: function () {
             var _this = this;
@@ -16,9 +20,40 @@ var swiper = (function () {
                     _this.index = target.getAttribute("index");
                     for (var i = 0; i < _this.bannerImg.length; i++) {
                         _this.bannerImg[i].className = "banner_img bannerImg";
+                        _this.circle_liAll[i].removeAttribute('class');
                     }
                     _this.bannerImg[_this.index].className = "banner_img bannerBlock";
+                    _this.circle_liAll[_this.index].className = "circle_active";
                 }
+            }
+        },
+        autoplay: function () {
+            var _this = this;
+            this.timer = setInterval(function () {
+                _this.nextplay();
+            }, 3000);
+        },
+        nextplay: function () {
+            var _this = this;
+            this.index++;
+            if (this.index > _this.bannerImg.length - 1) {
+                this.index = 0
+            }
+            for (var i = 0; i < _this.bannerImg.length; i++) {
+                _this.bannerImg[i].className = "banner_img bannerImg";
+                _this.circle_liAll[i].removeAttribute('class');
+            }
+            _this.bannerImg[_this.index].className = "banner_img bannerBlock";
+            _this.circle_liAll[_this.index].className = "circle_active";
+        },
+        stopplay: function () {
+            var _this = this;
+            this.banner.onmouseenter = function () {
+                clearInterval(_this.timer);
+            }
+            this.banner.onmouseleave = function () {
+                _this.autoplay();
+                
             }
         }
     }
