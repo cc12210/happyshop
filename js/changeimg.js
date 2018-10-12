dbfe091fcbe0c5a1b08000591c3e784d8756932e
cc12210 <2832913976@qq.com>
2018-10-12 17:45:36 +0800

    商品详情大图更换

---
+++ js/changeimg.js
@@ -0,0 +1,11 @@
+var imgUl = $('.gra_img'),
+    imgLiAll = $('.gra_img').children(),
+    showImg = $('.showImg'),
+    showBigImg = $('.showBigImg');
+imgUl.on('mouseenter', 'img', function () {
+    imgLiAll.removeClass('gra_active');
+    $(this).parent().addClass('gra_active');
+    console.log($(this)[0].src);
+    showImg[0].src = $(this)[0].src;
+    showBigImg[0].src = $(this)[0].src;
+})
