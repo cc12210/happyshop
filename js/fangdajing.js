2bc00a50d2f298e6c6ad3f1f9b35fa68eca94946
cc12210 <2832913976@qq.com>
2018-10-12 14:50:17 +0800

    jq完成了商品详情页的放大镜功能

---
+++ js/fangdajing.js
@@ -0,0 +1,48 @@
+var smallBox = $('.small_img'),
+    bigBox = $('.big_img'),
+    bigImg = $('.big_img img'),
+    mainBox = $('.shopinfo_l_img');
+mainBox.on('mouseenter', function () {
+    smallBox.show();
+    bigBox.show();
+});
+mainBox.mousemove(function (ev) {
+    var x = ev.clientX,
+        y = ev.clientY,
+        _x = mainBox.offset().left,
+        _y = mainBox.offset().top;
+    var mx = x - _x,
+        my = y - _y,
+        mw = smallBox.width() / 2,
+        mh = smallBox.height() / 2;
+    smallBox.css({
+        left: mx - mw + 'px',
+        top: my - mh + 'px'
+    });
+    var lw = smallBox.position().left,
+        lh = smallBox.position().top;
+    var maxW = mainBox.width() - smallBox.width(),
+        maxH = mainBox.width() - smallBox.width();
+    if (lw <= 0) {
+        smallBox.css('left', '0px');
+    } else if (lw >= maxW) {
+        smallBox.css('left', maxW + 'px');
+    }
+    if (lh <= 0) {
+        smallBox.css('top', '0px');
+    } else if (lh >= maxH) {
+        smallBox.css('top', maxH + 'px');
+    }
+    var lw = smallBox.position().left,
+        lh = smallBox.position().top,
+        bw = lw * 2,
+        bh = lh * 2;
+    bigImg.css({
+        left: -bw + 'px',
+        top: -bh + 'px'
+    });
+});
+mainBox.on('mouseleave', function () {
+    smallBox.hide();
+    bigBox.hide();
+});
