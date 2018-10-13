2dc91ada39f99957f25e39fb77b43a9538c9b961
cc12210 <2832913976@qq.com>
2018-10-13 15:14:51 +0800

    代码回退出现了bug,.修改

--- js/shop_car.js
+++ js/shop_car.js
@@ -11,49 +11,7 @@ $(function () {
         my_local = JSON.parse(my_local);
         var count = my_local.count;
         count_shop_w.val(parseInt(count));
-        want_price.html(parseInt(count_shop_w.val() * 698) + '.00');
+        want_price.html(parseInt(count_shop_w.val()*698)+".00");
         console.log(want_price.html());
     }
-    //购物车增加减少数目
-    var add_car = $('.add_car'),
-        add_btn = $('.add_btn'),
-        re_btn = $('.re_btn'),
-        imgSrc = $('.showImg'),
-        count = $('.count_shop');
-    add_btn.click(function () {
-        if (count.val() == "") {
-            count.val(1);
-        }
-        count.val(parseInt(count.val()) + 1);
-    });
-    re_btn.click(function () {
-        if (count.val() == "") {
-            count.val(1);
-        }
-        count.val(parseInt(count.val()) - 1);
-        if (count.val() <= 0) {
-            count.val(parseInt(count.val()) + 1);
-        }
-    });
-    count.on('input', function () {
-        if (count.val() >= 999) {
-            count.val(999);
-        } else if (count.val() < 0) {
-            count.val(1);
-        }
-    });
-    add_car.click(function () {
-        var shop_name = $.trim($('.detail_tit').html()),
-            new_price = $('.price_now_num').html(),
-            my_count = count.val();
-        var obj = {
-            "count": my_count
-        }
-        obj = JSON.stringify(obj);
-        window.localStorage.setItem("shop", obj);
-        var newObj = window.localStorage.getItem("shop");
-        newObj = JSON.parse(newObj);
-        console.log(newObj);
-        alert('添加成功,快去购物车页面结算吧');
-    });
 });
