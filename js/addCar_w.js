a8cae26d950a5853f523f009dce1697a1eb44aba
cc12210 <2832913976@qq.com>
2018-10-13 15:49:36 +0800

    购物车功能完善

--- js/addCar_w.js
+++ js/addCar_w.js
@@ -9,8 +9,12 @@ add_btn.click(function () {
     count.val(parseInt(count.val()) + 1);
     var newObj = window.localStorage.getItem('shop');
     newObj = JSON.parse(newObj);
-    console.log(newObj.count);
     want_price.html(parseInt(count.val() * 698) + ".00");
+    var newCount = {
+        "count": count.val()
+    }
+    newCount = JSON.stringify(newCount);
+    window.localStorage.setItem('shop', newCount);
 });
 re_btn.click(function () {
     if (count.val() == "") {
@@ -19,12 +23,16 @@ re_btn.click(function () {
     count.val(parseInt(count.val()) - 1);
     var newObj = window.localStorage.getItem('shop');
     newObj = JSON.parse(newObj);
-    console.log(newObj.count);
     want_price.html(parseInt(count.val() * 698) + ".00");
     if (count.val() <= 0) {
         count.val(parseInt(count.val()) + 1);
-        want_price.html("698.00");
+        want_price.html(parseInt(count.val() * 698) + ".00");
     }
+    var newCount = {
+        "count": count.val()
+    }
+    newCount = JSON.stringify(newCount);
+    window.localStorage.setItem('shop', newCount);
 });
 count.on('input', function () {
     if (count.val() >= 999) {
@@ -32,4 +40,12 @@ count.on('input', function () {
     } else if (count.val() < 0) {
         count.val(1);
     }
+    if (count.val() != "") {
+        want_price.html(parseInt(count.val() * 698) + ".00");
+    }
+    var newCount = {
+        "count": count.val()
+    }
+    newCount = JSON.stringify(newCount);
+    window.localStorage.setItem('shop', newCount);
 });
