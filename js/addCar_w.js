b92a357517f4b34c063bb42248a911c7a26ae336
cc12210 <2832913976@qq.com>
2018-10-13 15:39:08 +0800

    购物车界面数量总价变化

--- js/addCar_w.js
+++ js/addCar_w.js
@@ -1,19 +1,29 @@
 var add_btn = $('.add_btn_w'),
     re_btn = $('.re_btn_w'),
-    count = $('.count_shop_w');
+    count = $('.count_shop_w'),
+    want_price = $('.want_price');
 add_btn.click(function () {
     if (count.val() == "") {
         count.val(1);
     }
     count.val(parseInt(count.val()) + 1);
+    var newObj = window.localStorage.getItem('shop');
+    newObj = JSON.parse(newObj);
+    console.log(newObj.count);
+    want_price.html(parseInt(count.val() * 698) + ".00");
 });
 re_btn.click(function () {
     if (count.val() == "") {
         count.val(1);
     }
     count.val(parseInt(count.val()) - 1);
+    var newObj = window.localStorage.getItem('shop');
+    newObj = JSON.parse(newObj);
+    console.log(newObj.count);
+    want_price.html(parseInt(count.val() * 698) + ".00");
     if (count.val() <= 0) {
         count.val(parseInt(count.val()) + 1);
+        want_price.html("698.00");
     }
 });
 count.on('input', function () {
